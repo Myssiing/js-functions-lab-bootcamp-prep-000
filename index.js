6030503743ebaaf20244222560ae13f4067ba6f1
Tara Foulkrod <photochik02@hotmail.com>
2017-09-18 19:00:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-funtion happyHolidayTo (holiday, name) {
+funtion happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown (holiday, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
